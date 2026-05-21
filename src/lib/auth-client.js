cb44850247295a6d8237c5b036c2c4caa240ff1b
masumb30 <masum30.b@gmail.com>
2026-05-21 22:08:11 +0600

    url

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -4,7 +4,7 @@ import { createAuthClient } from "better-auth/react"
 import { jwtClient } from "better-auth/client/plugins"
 export const authClient = createAuthClient({
 
-    baseURL: process.env.BETTER_AUTH_URL,
+    baseURL: "https://assign9frontend.vercel.app",
     plugins: [
         jwtClient()
     ]
